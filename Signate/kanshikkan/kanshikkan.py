aed8986a996fc6e566b19f15a4ad5ae25b6b1308
myo-kun <54165278+myo-kun@users.noreply.github.com>
2020-09-04 01:11:38 +0900

    add new data for competiton

--- Signate/kanshikkan/kanshikkan.py
+++ Signate/kanshikkan/kanshikkan.py
@@ -1,30 +1,29 @@
-import csv
-import scipy as sp
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 import seaborn as sns
-import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import KFold, StratifiedKFold, cross_val_score
+
 sns.set()
 
-def preprocess_sex(df):
-    df['Gender'] = df['Gender'].replace({'Male':0, 'Female': 1})
 
-train_data = pd.read_csv('train.csv')
-test_data = pd.read_csv('test.csv')
-out_id = test_data['id']
-train_data['AG_ratio'] = train_data['AG_ratio'].fillna(train_data['AG_ratio'].mean())
+def preprocess_sex(df):
+    df["Gender"] = df["Gender"].replace({"Male": 0, "Female": 1})
+
+
+train_data = pd.read_csv("train.csv")
+test_data = pd.read_csv("test.csv")
+
+out_id = test_data["id"]
+train_data["AG_ratio"] = train_data["AG_ratio"].fillna(train_data["AG_ratio"].mean())
 preprocess_sex(train_data)
-y_train = train_data['disease']
-X_train = train_data.drop(columns=['id', 'Age', 'disease', 'Alb', 'AG_ratio', 'Gender', 'TP'])
-X_test = test_data.drop(columns=['id', 'Age', 'Alb', 'AG_ratio', 'Gender', 'TP'])
+y_train = train_data["disease"]
+X_train = train_data.drop(
+    columns=["id", "Age", "disease", "Alb", "AG_ratio", "Gender", "TP"]
+)
+X_test = test_data.drop(columns=["id", "Age", "Alb", "AG_ratio", "Gender", "TP"])
 model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 
 output = dict(zip(out_id.to_list(), predictions))
-submission = pd.DataFrame.from_dict(output,orient='index')
-submission.to_csv('submission.csv', header=False)
+submission = pd.DataFrame.from_dict(output, orient="index")
+submission.to_csv("submission.csv", header=False)
